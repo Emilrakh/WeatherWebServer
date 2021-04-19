5109cfdd8fcd9f4030b2af0ae8f4536dd293b8fd
emilrakh <emilrakh@gmail.com>
2021-04-19 23:06:01 +0500

    fixed

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express from "express";
 import {requestTime, logger, errorHandler} from "./middlewares.js";
-// import mongodb from "mongodb";
-// import { mongodbConnection } from "./db.js";
 import serverRoutes from "./controller.js";
 import cors from "cors";
 
@@ -9,29 +7,15 @@ const PORT = process.env.PORT ?? process.env.HOST_PORT;
 const CORS_PORT = process.env.CORS_PORT;
 const app = express();
 
-// app.use(cors());
-//
-// app.get('/', function (req, res, next) {
-//     res.json({msg: 'This is CORS-enabled for all origins!'});
-// })
-//
-// app.listen(CORS_PORT, function () {
-//     console.log(`CORS-enabled web server listening on port ${CORS_PORT}`);
-// })
+app.use(cors());
 
-// async function main(){
-//     const client = new mongodb.MongoClient(mongodbConnection.url, mongodbConnection.options);
-//     try {
-//         await client.connect();
-//         await mongodb.listDatabases(client);
-//     } catch (e) {
-//         console.error(e);
-//     } finally {
-//         await client.close();
-//     }
-// }
-//
-// main().catch(console.error);
+app.get('/', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'});
+})
+
+app.listen(CORS_PORT, function () {
+    console.log(`CORS-enabled web server listening on port ${CORS_PORT}`);
+})
 
 app.use(errorHandler);
 app.use(requestTime);
