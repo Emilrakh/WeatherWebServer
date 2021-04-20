b214056226f9e4b069dc2e58ee0ea9523543c8c8
emilrakh <emilrakh@gmail.com>
2021-04-21 00:14:23 +0500

    smth

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'});
-})
+// app.get('/', function (req, res, next) {
+//     res.json({msg: 'This is CORS-enabled for all origins!'});
+// })
 
 app.listen(CORS_PORT, function () {
     console.log(`CORS-enabled web server listening on port ${CORS_PORT}`);
