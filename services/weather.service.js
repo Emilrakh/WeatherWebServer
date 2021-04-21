e89d8bdc75a5a2ce2cde1de49df15cf8d1b1c4fd
emilrakh <emilrakh@gmail.com>
2021-04-22 03:08:35 +0500

    fixed url and secret

--- services/weather.service.js
+++ services/weather.service.js
@@ -3,10 +3,12 @@ import fetch from "node-fetch";
 
 dotenv.config();
 
-const url = process.env.API_URL || secret.API_URL + "&appid=" + process.env.API_KEY || secret.API_KEY;
+const url = process.env.API_URL || secret.API_URL
+const key = process.env.API_KEY || secret.API_KEY
+const full_url = url + "&appid=" + key;
 
 export function fetchWeatherByCity(cityName) {
-    return fetch(url + "&q=" + cityName)
+    return fetch(full_url + "&q=" + cityName)
         .then( (res) => {
             const data = res.json();
             return data;
@@ -14,7 +16,7 @@ export function fetchWeatherByCity(cityName) {
 }
 
 export function fetchWeatherByCoordinate(lat, lon) {
-    return fetch(url + "&lat=" + lat + "&lon=" + lon)
+    return fetch(full_url + "&lat=" + lat + "&lon=" + lon)
         .then( (res) => {
             const data = res.json();
             return data;
