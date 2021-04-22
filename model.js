eb1c9f83d25dab064b0987b85cf4a0389193846d
emilrakh <emilrakh@gmail.com>
2021-04-22 20:44:55 +0500

    fixed

--- model.js
+++ model.js
@@ -2,7 +2,12 @@
 import pkg from 'mongoose';
 const {Schema, model} = pkg;
 
-const favouriteCities = new Schema({cityName: {type: "string"}});
+const favouriteCities = new Schema({
+    cityName: {
+        type: "string",
+        required: true,
+        unique: false,
+    }});
 const cityModel =  model("favouriteCities", favouriteCities);
 
 export default cityModel
